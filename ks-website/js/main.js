4e93156a1e54af562d6a590c873146ab08d83db8
KarismaSoni <karismavsoni93@gmail.com>
2015-10-06 15:38:26 -0500

    initial Commit
    Final project review html, css, js, jquery

---
+++ ks-website/js/main.js
@@ -0,0 +1,19 @@
+jQuery(document).ready(function($){
+	//open the lateral panel
+	$('ul.tabs li').on('click', function(e){
+		e.preventDefault();
+		var tabId = $(this).attr('data-tab');
+		$('.ks-main-content').addClass('ks-slide-left');
+		$('ul.tabs li').removeClass('is-visible');
+		$('.ks-sub-pages').removeClass('is-visible');
+		$("#" + tabId).addClass('is-visible');
+	});
+	//clode the lateral panel
+	$('.ks-sub-pages').on('click', function(event){
+		if( $(event.target).is('.ks-sub-pages') || $(event.target).is('.ks-sub-pages-close') ) { 
+			$('.ks-sub-pages').removeClass('is-visible');
+			$('.ks-main-content').removeClass('ks-slide-left');
+			event.preventDefault();
+		}
+	});
+});
